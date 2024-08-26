30a68aa05c9ae3d17329c023f3ba6c88b082bde6
Himani0002 <himanigohil0002@gmail.com>
2024-08-26 18:05:42 +0530

    HG | 26/8/24 | DOCS

---
+++ Docs Mini App/docs/src/components/Forground.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './Card'
+
+const Forground = () => {
+  return (
+    <>
+      <div className="fixed top-0 left-0 z-[3] w-full h-full">
+        <Card />
+      </div>
+
+
+    </>
+  )
+}
+
+export default Forground
