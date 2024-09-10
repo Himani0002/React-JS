06b339b8b66b0a6c0eba2d22e072247761c4bfae
Himani0002 <himanigohil0002@gmail.com>
2024-09-10 18:52:35 +0530

    HG | 10/9/24 | Imagegen

---
+++ Image Engine/script.js
@@ -0,0 +1,4 @@
+const searchFrom = document.getElementById("Search-form");
+const searchBox = document.getElementById("Search-box");
+const searchResult = document.getElementById("Search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
