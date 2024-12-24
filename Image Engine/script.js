560435a9b9cb5ab820cb213290d2174fb6644c10
Himani0002 <himanigohil0002@gmail.com>
2024-12-24 20:45:41 +0530

    HG | 24/12/24 | React

--- Image Engine/script.js
+++
@@ -1,4 +0,0 @@
-const searchFrom = document.getElementById("Search-form");
-const searchBox = document.getElementById("Search-box");
-const searchResult = document.getElementById("Search-result");
-const showMoreBtn = document.getElementById("show-more-btn");
--- threads_clone/app/(Root)/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main>
-      <h1>threads_clone</h1>
-    </main>
-  );
-}
